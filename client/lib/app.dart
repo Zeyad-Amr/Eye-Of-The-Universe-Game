42129680669b343064772b723c188725a77fc4f0
Zeyad Amr <68791488+Zeyad-Amr@users.noreply.github.com>
2022-10-02 21:47:04 +0200

    home ui

--- client/lib/app.dart
+++ client/lib/app.dart
@@ -1,5 +1,4 @@
 import 'package:client/home.dart';
-import 'package:client/login.dart';
 import 'package:flutter/material.dart';
 
 class App extends StatefulWidget {
@@ -12,6 +11,6 @@ class App extends StatefulWidget {
 class _AppState extends State<App> {
   @override
   Widget build(BuildContext context) {
-    return const Login();
+    return const Home();
   }
 }
--- client/lib/home.dart
+++ client/lib/home.dart
@@ -14,124 +14,236 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   Data data = Data();
+  // Initial Selected Value
+  String dropdownvalue = 'Level 1';
+  int level = 1;
+
+  // List of items in our dropdown menu
+  var items = [
+    'Level 1',
+    'Level 2',
+    'Level 3',
+    'Level 4',
+    'Level 5',
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.blue[900],
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: SizedBox(
-                width: MediaQuery.of(context).size.width * 0.5,
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                      foregroundColor: Colors.black,
-                      backgroundColor: Colors.white,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(15)),
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 20, vertical: 15)),
-                  onPressed: () {
-                    Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (context) => Discovery(data: data.data[0]),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        color: Colors.white,
                       ),
-                    );
-                  },
-                  child: const Text(
-                    "Discovery",
-                    style: TextStyle(fontSize: 20),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          'Score: ${(level * 500)}',
+                          style: const TextStyle(fontSize: 20),
+                        ),
+                      )),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: DropdownButton(
+                      value: dropdownvalue,
+                      borderRadius: BorderRadius.circular(15),
+                      // Down Arrow Icon
+                      icon: const Icon(Icons.keyboard_arrow_down),
+                      style: const TextStyle(
+                        color: Colors.black,
+                      ),
+                      // Array list of items
+                      items: items.map((String items) {
+                        return DropdownMenuItem(
+                          value: items,
+                          child: Text(items),
+                        );
+                      }).toList(),
+                      // After selecting the desired option,it will
+                      // change button value to selected value
+                      onChanged: (String? newValue) {
+                        setState(() {
+                          dropdownvalue = newValue!;
+                          if (dropdownvalue == 'Level 1') {
+                            level = 1;
+                          } else if (dropdownvalue == 'Level 2') {
+                            level = 2;
+                          } else if (dropdownvalue == 'Level 3') {
+                            level = 3;
+                          } else if (dropdownvalue == 'Level 4') {
+                            level = 4;
+                          } else if (dropdownvalue == 'Level 5') {
+                            level = 5;
+                          }
+                        });
+                      },
+                    ),
                   ),
-                ),
+                ],
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: SizedBox(
-                width: MediaQuery.of(context).size.width * 0.5,
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                      foregroundColor: Colors.black,
-                      backgroundColor: Colors.white,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(15)),
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 20, vertical: 15)),
-                  onPressed: () {
-                    Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (context) => PuzzleWidget(
-                          gridSize: 2,
-                          timeInSec: 20,
-                          img: data.data[0]['jwst'],
-                          title: data.data[0]['image'],
-                          description: data.data[0]['description'],
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    const SizedBox(
+                      height: 50,
+                    ),
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.6,
+                      child: const Image(
+                        image: AssetImage('assets/logo.png'),
+                      ),
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.fromLTRB(0, 30, 0, 0),
+                      child: Text(
+                        "Eye of The Universe",
+                        style: TextStyle(
+                            fontSize: 30,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.fromLTRB(0, 3, 0, 30),
+                      child: Text(
+                        "Let's Discover the Space",
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.white,
                         ),
                       ),
-                    );
-                  },
-                  child: const Text(
-                    "Puzzle",
-                    style: TextStyle(fontSize: 20),
-                  ),
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: SizedBox(
-                width: MediaQuery.of(context).size.width * 0.5,
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                      foregroundColor: Colors.black,
-                      backgroundColor: Colors.white,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(15)),
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 20, vertical: 15)),
-                  onPressed: () {
-                    Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (context) => const FlippedCards(size: 3),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.6,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              foregroundColor: Colors.black,
+                              backgroundColor: Colors.white,
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(15)),
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 20, vertical: 15)),
+                          onPressed: () {
+                            Navigator.of(context).push(
+                              MaterialPageRoute(
+                                builder: (context) =>
+                                    Discovery(data: data.data[level - 1]),
+                              ),
+                            );
+                          },
+                          child: const Text(
+                            "Discovery",
+                            style: TextStyle(fontSize: 20),
+                          ),
+                        ),
                       ),
-                    );
-                  },
-                  child: const Text(
-                    "Flipped Cards",
-                    style: TextStyle(fontSize: 20),
-                  ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.6,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              foregroundColor: Colors.black,
+                              backgroundColor: Colors.white,
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(15)),
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 20, vertical: 15)),
+                          onPressed: () {
+                            Navigator.of(context).push(
+                              MaterialPageRoute(
+                                builder: (context) => PuzzleWidget(
+                                  gridSize: level + 1,
+                                  timeInSec: 20 + ((level - 1) * 40),
+                                  img: data.data[level - 1]['jwst'],
+                                  title: data.data[level - 1]['image'],
+                                  description: data.data[level - 1]
+                                      ['description'],
+                                ),
+                              ),
+                            );
+                          },
+                          child: const Text(
+                            "Puzzle",
+                            style: TextStyle(fontSize: 20),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.6,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              foregroundColor: Colors.black,
+                              backgroundColor: Colors.white,
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(15)),
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 20, vertical: 15)),
+                          onPressed: () {
+                            Navigator.of(context).push(
+                              MaterialPageRoute(
+                                builder: (context) => FlippedCards(
+                                    size: level == 1
+                                        ? 2
+                                        : level == 2
+                                            ? 3
+                                            : level == 4
+                                                ? 5
+                                                : 5),
+                              ),
+                            );
+                          },
+                          child: const Text(
+                            "Flipped Cards",
+                            style: TextStyle(fontSize: 20),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.6,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              foregroundColor: Colors.black,
+                              backgroundColor: Colors.white,
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(15)),
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 20, vertical: 15)),
+                          onPressed: () {
+                            Navigator.of(context).push(
+                              MaterialPageRoute(
+                                  builder: (context) => const FocusWidget()),
+                            );
+                          },
+                          child: const Text(
+                            "PSF",
+                            style: TextStyle(fontSize: 20),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: SizedBox(
-                width: MediaQuery.of(context).size.width * 0.5,
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                      foregroundColor: Colors.black,
-                      backgroundColor: Colors.white,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(15)),
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 20, vertical: 15)),
-                  onPressed: () {
-                    Navigator.of(context).push(
-                      MaterialPageRoute(
-                          builder: (context) => const FocusWidget()),
-                    );
-                  },
-                  child: const Text(
-                    "PSF",
-                    style: TextStyle(fontSize: 20),
-                  ),
-                ),
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
